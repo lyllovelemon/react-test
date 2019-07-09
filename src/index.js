f24c9851ee5a42611e6c80f981b762339b5aa5d2
liyili <732193462@qq.com>
2019-07-09 15:49:45 +0800

    feat:event handler

--- src/index.js
+++ src/index.js
@@ -5,37 +5,28 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
 
-  class Clock extends React.Component{
-	  constructor(props){
-		  super(props);
-		  this.state={date:new Date()};
-	  }
-	  componentDidMount(){
-		  this.timerId=setInterval(
-			  ()=>this.tick(),
-			  1000
-		  );
-	  }
-	  componentWillUnmount(){
-		  clearInterval(this.timerId);
-	  }
-	  tick(){
-		  this.setState({
-			  date:new Date()
-		  });
-	  }
-	  render(){
-		return (
-			<div>
-			  <h1>Hello, world!</h1>
-			  <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-			</div>
-		  );
-	  }
-  }
-  
+class Toggle extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={isToggleOn:true};
+
+		this.handleClick=this.handleClick.bind(this);
+	}
+	handleClick(){
+		this.setState(state=>({
+			isToggleOn:!this.state.isToggleOn
+		}))
+	}
+	render(){
+		return(
+			<button onClick={this.handleClick}>
+				{this.state.isToggleOn?'ON':'OFF'}
+			</button>
+		)
+	}
+}
   ReactDOM.render(
-	<Clock />,
+	<Toggle />,
 	document.getElementById('root')
 	);
   
