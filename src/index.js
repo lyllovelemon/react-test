04fea81af1435a9c258c70c33969f5752173e370
liyili <732193462@qq.com>
2019-07-10 19:43:27 +0800

    feat:extends

--- src/index.js
+++ src/index.js
@@ -16,8 +16,38 @@ function Dialog(props){
 			<p className="Dialog-message">
 				{props.message}
 			</p>
+			{props.children}
 		</FancyBorder>
 	)
+}
+class SignUpDialog extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleChange=this.handleChange.bind(this);
+		this.handleSignUp=this.handleSignUp.bind(this);
+		this.state={login:''};
+	}
+	render(){
+		return(
+			<Dialog 
+			title="Mars Exploration Program"
+			message="How should we refer to you?"
+			>
+				<input value={this.state.login}
+				onChange={this.handleChange}
+				/>
+				<button onClick={this.handleSignUp}>
+					Sign me up!
+				</button>
+			</Dialog>
+		)
+	}
+	handleChange(e){
+		this.setState({login:e.target.value});
+	}
+	handleSignUp(){
+		alert(`Welcome aboard,${this.state.login}!`);
+	}
 }
 function WelcomeDialog(){
 	return(
