261f1d67171be8af2c68947268eb5b19dc5ec1fa
liyili <732193462@qq.com>
2019-07-10 17:13:44 +0800

    feat:add celsius

--- src/index.js
+++ src/index.js
@@ -1,39 +1,91 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
 
-class FlavorForm extends React.Component{
+function BoilingWatar(props){
+	if(props.celsius>=100){
+		return (<p>The water would boil.</p>)
+	}
+	return (<p>The water would not boil.</p>)
+}
+class Calculator extends React.Component{
 	constructor(props){
 		super(props);
-		this.state={value:'coconut'};
-		this.handleChange=this.handleChange.bind(this);
-		this.handleSubmit=this.handleSubmit.bind(this);
+		this.handleCelsiusChange=this.handleCelsiusChange.bind(this);
+		this.handleFahrenheitChange=this.handleFahrenheitChange.bind(this);
+		this.state={temperature:'',scale:'c'};
 	}
-	handleChange(event){
-		this.setState({value:event.target.value})
+	handleCelsiusChange(temperature){
+		this.setState({
+			scale:'c',temperature
+		})
 	}
-	handleSubmit(event){
-		alert("你喜欢的风味是"+this.state.value);
-		event.preventDefault();
+	handleFahrenheitChange(temperature){
+		this.setState({
+			scale:'f',temperature
+		})
 	}
 	render(){
+		const scale=this.state.scale;
+		const temperature=this.state.temperature;
+		const celsius=scale==='f'?tryConvert(temperature,toCelsius):temperature;
+		const fahreheit=scale==='c'?tryConvert(temperature,toFahreheit):temperature;
 		return(
-			<form onSubmit={this.handleSubmit}>
-				<label>
-					选择你喜欢的风味:
-					<select value={this.state.value} onChange={this.handleChange}>
-						<option value="grapefruit">葡萄柚</option>
-						<option value="lime">柠檬</option>
-						<option value="coconut">椰子</option>
-						<option value="mango">芒果</option>	
-					</select>
-				</label>
-				<input type="submit" value="提交"/>
-			</form>
+			<div>
+				<TemperatureInput 
+				scale="c"
+				temperature={celsius}
+				onTemperatureChange={this.handleCelsiusChange}/>
+				<TemperatureInput 
+				scale="f"
+				temperature={fahreheit}
+				onTemperatureChange={this.handleFahrenheitChange}/>
+				<BoilingWatar 
+				celsius={parseFloat(celsius)}/>
+			</div>
 		)
 	}
 }
+const scaleNames={
+	c:'Celsius',
+	f:'Fahreneit'
+}
+class TemperatureInput extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleChange=this.handleChange.bind(this);
+	}
+	handleChange(e){
+		this.props.onTemperatureChange(e.target.value);
+	}
+	render(){
+		const temperature=this.props.temperature;
+		const scale=this.props.scale;
+		return(
+			<fieldset>
+				<legend>Enter temperature in {scaleNames[scale]}:</legend>
+				<input value={temperature}
+				onChange={this.handleChange}/>
+			</fieldset>
+		)
+	}
+}
+function toCelsius(fahreheit){
+	return (fahreheit-32)*5/9;
+}
+function toFahreheit(celsius){
+	return (celsius*9)/5+32;
+}
+function tryConvert(temperature,convert){
+	const input=parseFloat(temperature);
+	if(Number.isNaN(input)){
+		return '';
+	}
+	const output=convert(input);
+	const rounded=Math.round(output*1000)/1000;
+	return rounded.toString();
+}
 ReactDOM.render(
-	<FlavorForm/>,
+	<Calculator/>,
 	document.getElementById('root')
 )
 //  function Comment(props){
