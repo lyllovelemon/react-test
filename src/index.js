3c2c687cc2363cb843e6038842db35a00b0f39d4
liyili <732193462@qq.com>
2019-07-10 15:22:15 +0800

    feat:form handler

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
 
-function Blog(props){
-	const sidebar=(
-		<ul>
-			{props.posts.map((post)=>
-				<li key={post.id}>
-					{post.title}
-				</li>
-				)}
-		</ul>
-	);
-	const content=props.posts.map((post)=>
-		<div key={post.id}>
-			<h3>{post.title}</h3>
-			<p>{post.content}</p>	
-		</div>
-	);
-	return(
-		<div>
-			{sidebar}
-			<hr/>
-			{content}
-		</div>
-	)
+class NameForm extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={value:''};
+		this.handleChange=this.handleChange.bind(this);
+		this.handleSubmit=this.handleSubmit.bind(this);
+	}
+	handleChange(event){
+		this.setState({value:event.target.value});
+	}
+	handleSubmit(event){
+		alert("提交的名字"+this.state.value);
+		event.preventDefault();
+	}
+	render(){
+		return(
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					名字:
+					<input type='text' value={this.state.value} onChange={this.handleChange}/>
+				</label>
+				<input type='submit' value="提交"/>	
+			</form>
+		)
+	}
 }
-const posts=[
-	{id:1,title:'hello lemon',content:'welcome to lemon`s world'},
-	{id:2,title:'Ins',content:'there are many ins'}
-]
 ReactDOM.render(
-	<Blog posts={posts}/>,
+	<NameForm/>,
 	document.getElementById('root')
 )
 //  function Comment(props){
